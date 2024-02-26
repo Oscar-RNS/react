d603ad554ca1a615502cc6af9ae07bb9d88020ec
Oscar Nava <111458908+Oscar-RNS@users.noreply.github.com>
2024-02-25 20:40:27 -0600

    Se aplico router navigation botón inicio

---
+++ src/components/home.tsx
@@ -0,0 +1,15 @@
+
+/*interface Props {     
+  formDatos: string;
+}
+
+const Home = ({formDatos} : Props) => {*/
+const Home = () => {    
+    return (
+    <>   
+      Hola
+    </>
+    ) 
+}
+
+export default Home
